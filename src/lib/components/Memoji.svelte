46284f90f606fa3e3d456e01d42a84fe38e1ae26
Mert Doğu <mertdoguonder@protonmail.com>
2023-05-17 10:06:33 +0300

    feat: animation to memoji

--- src/lib/components/Memoji.svelte
+++ src/lib/components/Memoji.svelte
@@ -7,7 +7,7 @@
     import memoji6 from '$lib/images/memoji/memoji6.png';
 
 
-    const numbers = [
+    const emojis = [
     memoji1,
     memoji2,
     memoji3,
@@ -16,63 +16,44 @@
     memoji6
 ];
 
-var selectedNumber = memoji1
+let keycount = 0;
 
-// 3 saniye aralıklarla yeni sayıları seçelim
-setTimeout(function selectRandomNumber() {
-  // Eğer dizi boşsa işlemi sonlandıralım
-  if (numbers.length === 0) {
-    return;
-  }
-
-  // Yeni bir sayı seçelim
-  selectedNumber = numbers[Math.floor(Math.random() * numbers.length)];
-
-  // Seçilen sayıyı diziden çıkartalım
-  var index = numbers.indexOf(selectedNumber);
-  if (index > -1) {
-    numbers.splice(index, 1);
-  }
-
-  // 3 saniye sonra tekrar yeni bir sayı seçmek için fonksiyonu çağıralım
-  setTimeout(selectRandomNumber, 3000);
-}, 3000);
-
-/*let randomEmoji = null
-let previousEmoji = null;
-let count = 0;
-
-function getRandomEmoji() {
-    randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-    while (randomEmoji === previousEmoji) {
-            randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-            console.log('same emoji')
-
+function keyemoji(){
+    if (keycount == 5){
+        keycount = 0
+    } else {
+        keycount = keycount + 1
     }
-            previousEmoji = randomEmoji;
-            return randomEmoji;
-
 }
-
-function setRandomEmoji() {
-    randomEmoji = getRandomEmoji();
-    setTimeout(() => {
-        setRandomEmoji();
-    }, 3000);
-}
-
-if (count == 0) {
-    setRandomEmoji();
-}
-*/
-
-
+setInterval(() => {
+    keyemoji()
+}, 10000)
 
 </script>
 
-<div class="h-[5rem] w-[5rem] p-1 bg-[#808080]/5 rounded-full relative flex justify-center items-center">
-    <img src={selectedNumber} alt="Mert Dogu" />
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div on:click={keyemoji} class="h-[5rem] w-[5rem] p-1 bg-[#808080]/5 rounded-full relative flex justify-center items-center">
+    {#key emojis[keycount]}
+        <img src={emojis[keycount]} class="imagesy" alt="Mert Dogu" />
+    {/key}
     <div class="absolute flex justify-center right-0 bottom-0 bg-white rounded-full h-7 w-7 items-center">
         <p class="text-[1.2rem]">🎉</p>
     </div>
 </div>
+
+<style>
+    @keyframes fadein {
+        0% {
+            opacity: 0;
+        }
+
+        100% {
+            opacity: 1;
+        }
+    }
+
+    .imagesy {
+        animation: fadein 1s;
+    }
+
+</style>
