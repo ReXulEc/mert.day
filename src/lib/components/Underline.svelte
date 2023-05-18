556062400f901936f7eb71ca68a0457b7d3ed6eb
Mert Doğu <mertdoguonder@protonmail.com>
2023-05-19 00:20:49 +0300

    feat: added arrow types

--- src/lib/components/Underline.svelte
+++ src/lib/components/Underline.svelte
@@ -1,15 +1,24 @@
 <script>
 export let href
 export let text = 'Take me to the magic land'
+export let arrowstyle = 'default'
+
+let arrow = {
+  default: 'M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25',
+  right: "M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+}
+
+//{arrow[arrowstyle]}
+
 </script>
 
-<a href={href} class="hover-underline-animation">
-    <div class="flex items-center space-x-2 text-white " href="">
+<a href={href} class="flex items-center space-x-2 ">
+    <div class="hover-underline-animation text-white " href="">
         <span class="font-medium">{text}</span>
-        <svg aria-hidden="true" fill="none" stroke="white" height='15px' stroke-width="3" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" stroke-linecap="round" stroke-linejoin="round"></path>
-      </svg>
     </div>
+    <svg aria-hidden="true" fill="none" stroke="white" height='15px' stroke-width="3" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path d="{arrow[arrowstyle]}" stroke-linecap="round" stroke-linejoin="round"></path>
+    </svg>
 </a>
 
 <style>
@@ -24,7 +33,7 @@ export let text = 'Take me to the magic land'
     position: absolute;
     width: 100%;
     transform: scaleX(1);
-    height: 2px;
+    height: 1.5px;
     bottom: 0;
     left: 0;
     background-color: #f1f1f1;
