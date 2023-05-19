2887a29a1292c3059d4203d57d6e27a4a19035eb
Mert Doğu <mertdoguonder@protonmail.com>
2023-05-20 00:44:31 +0300

    feat: changed place and vis

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,8 +1,10 @@
-<div class="flex w-full justify-center text-lg md:bg-black/0 bg-black/5 backdrop-blur-md md:py-16 py-5">
+<div class="flex w-full justify-center text-lg md:bg-black/0  backdrop-blur-md md:py-16 py-5">
     <div class="flex flex-row justify-between md:w-8/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 w-9/12">
         <p>Mert Dogu</p>
-        <p class="texts-05">Projects</p>
-        <p class="texts-05">About</p>
-        <p class="texts-05">Contact</p>
+        <div class=" space-x-6 md:flex hidden">
+            <p class="texts-05">Projects</p>
+            <p class="texts-05">About</p>
+            <p class="texts-05">Contact</p>
+        </div>
     </div>
 </div>
