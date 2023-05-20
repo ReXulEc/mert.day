fa82993ab9e5671dfa3c855e67c18c1489b1b79f
Mert Doğu <mertdoguonder@protonmail.com>
2023-05-20 12:28:11 +0300

    feat: change width

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,16 @@
 <script>
-	import "../app.css";
+  import "../app.css";
 	import Navbar from '$lib/components/Navbar.svelte';
+</script>
 
-  </script>
 
 <div>
   <div class="md:static sticky top-0 z-50">
     <Navbar />
   </div>
 <div class="flex justify-center md:pb-20 pb-10">
-  <div class="md:w-8/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 w-9/12">
-    <slot />
+  <div class="md:w-9/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 w-10/12">
+      <slot />
   </div>
 </div>
 </div>
