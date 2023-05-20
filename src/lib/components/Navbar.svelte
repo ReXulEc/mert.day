255c97f8c31116faae35e0e820d8a7f2693cab20
Mert Doğu <mertdoguonder@protonmail.com>
2023-05-20 12:28:59 +0300

    feat: navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,10 +1,21 @@
-<div class="flex w-full justify-center text-lg md:bg-black/0  backdrop-blur-md md:py-16 py-5">
-    <div class="flex flex-row justify-between md:w-8/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 w-9/12">
-        <p>Mert Dogu</p>
+<script>
+</script>
+
+
+<div class="flex w-full justify-center text-lg md:py-16 py-5 md:bg-black/0 bg-[#2A2A2A]">
+    <div class="flex flex-row justify-between md:w-9/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 w-10/12  ">
+        <a href="/" class="md:visible invisible text-white">Mert Dogu</a>
         <div class=" space-x-6 md:flex hidden">
-            <p class="texts-05">Projects</p>
-            <p class="texts-05">About</p>
-            <p class="texts-05">Contact</p>
+          <a href='/about' class="texts-05">About</a>
+            <a href='/projects' class="texts-05">Projects</a>
+            <a class="texts-05">Contact</a>
         </div>
+
+
+          <svg class="flex md:hidden h-8 text-white/80" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path clip-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z" fill-rule="evenodd"></path>
+          </svg>
     </div>
 </div>
+
+
