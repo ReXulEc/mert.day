d0838f22f911f1da0fac41cb49a6b241c6cce8f6
Mert Doğu <mertdoguonder@protonmail.com>
2023-05-28 01:26:14 +0300

    refactor: update style

--- src/lib/components/Toast.svelte
+++ src/lib/components/Toast.svelte
@@ -9,7 +9,7 @@
 	 */
 	export let message;
 
-	const originalClassList = 'p-3 px-4 bg-white/10 rounded-xl flex items-center space-x-2.5';
+	const originalClassList = ' p-3 px-4 max-w-full md:max-w-[400px] lg:max-w-[500px] bg-[#404040] rounded-xl flex items-center space-x-2.5';
 
 	if ($$restProps.class) {
 		$$restProps.class += ` ${originalClassList}`;
@@ -19,6 +19,6 @@
 </script>
 
 <div {...$$restProps}>
-	<p class="text-lg">{emoji} |</p>
+	<p class="text-lg">{emoji}</p>
 	<p class="text-white text-md">{message}</p>
 </div>
