6b5cf3d211840ebc3e711ad390ce9226952fdd51
Mert Doğu <mertdoguonder@protonmail.com>
2023-05-28 01:24:38 +0300

    feat: change rate limit

--- src/limiter.js
+++ src/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit')
 
 const apiLimiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 1, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 })
 
