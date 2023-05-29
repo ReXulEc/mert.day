dd0222844dd1241e22b8310d3b245f34e60c3dec
Mert Doğu <mertdoguonder@protonmail.com>
2023-05-29 18:14:24 +0300

    feat: add page transition

---
+++ src/routes/+layout.js
@@ -0,0 +1,3 @@
+export async function load({ fetch, url }) {
+    return { url: url.href }
+   }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import '../app.css';
 	import Navbar from '$lib/components/Navbar.svelte';
+
+	 export let data;
+	 import PageTransition from '$lib/components/PageTransition.svelte';
 </script>
 
 <div>
@@ -9,7 +12,9 @@
 	</div>
 	<div class="flex justify-center md:pb-20 pb-10">
 		<div class="md:w-9/12 lg:w-7/12 xl:w-6/12 2xl:w-5/12 w-10/12">
-			<slot />
+			<PageTransition url={data.url}>
+				<slot></slot>
+			</PageTransition>
 		</div>
 	</div>
 	<div class="fixed bottom-0 right-0 m-5 space-y-3" id="toast-container" />
