5ca0302dd7d5ad8a96f940913dde5002f68a195a
Mert Doğu <mertdoguonder@protonmail.com>
2023-06-03 00:01:15 +0300

    add: jwt

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const apiLimiter = require('./limiter.js');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
@@ -14,13 +15,20 @@ const mailSchema = new mongoose.Schema({
   email: String,
 });
 
-const Mail = mongoose.model('mails', mailSchema);
+const Mail = mongoose.model('maillist', mailSchema);
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({ origin: '*' }));
+app.use(cors( { origin: '*' }));
 
+function generateAccessToken(data) {
+  return jwt.sign(data, process.env.TOKEN_SECRET, { expiresIn: '168h' });
+}
+
+app.get('/', (req, res) => {
+  res.status(200).json({success: true, message: 'Welcome to the API!'});
+});
 
 app.post('/mail', apiLimiter, async (req, res) => {
   const { email } = req.body;
@@ -31,9 +39,33 @@ app.post('/mail', apiLimiter, async (req, res) => {
                 console.log('[USER ERROR] ' + mail.email + ' already exists!');
                 res.status(200).json({success: false, message: 'This email is already registered.'});
             } else {
-                const mail = new Mail({ email });
+                res.status(200).json({success: true, message: 'Confirm your email address to subscribe.'});
+                console.log(generateAccessToken({ email: email }))
+            }
+        });
+      } catch (err) {
+        console.error('[INVALID ERROR]', err);
+        res.status(400).json({success: false, message: 'An invalid error occurred. Please try again later.'});
+      }
+});
+
+app.get('/confirm/:auth', apiLimiter, async (req, res) => {
+  let emailconfirm = '';
+  jwt.verify(req.params.auth, process.env.TOKEN_SECRET, (err, verifiedJwt) => {
+    if(err){
+      res.send(err.message)
+    }else{
+      emailconfirm = verifiedJwt.email;
+      try {
+        Mail.findOne({ 'email': emailconfirm }).then(function (mail) {
+            console.log(mail);
+            if (mail) {
+                console.log('[USER ERROR] ' + emailconfirm + ' already exists!');
+                res.status(200).json({success: false, message: 'This email is already registered.'});
+            } else {
+                const mail = new Mail({ emailconfirm });
                 mail.save();
-                console.log('[SUCCESS] New user:', mail.email);
+                console.log('[SUCCESS] New user:', emailconfirm);
                 res.status(200).json({success: true, message: 'Thanks for signing up! You can always unsubscribe by clicking the link at the bottom of our emails.'});
             }
         });
@@ -41,6 +73,9 @@ app.post('/mail', apiLimiter, async (req, res) => {
         console.error('[INVALID ERROR]', err);
         res.status(400).json({success: false, message: 'An invalid error occurred. Please try again later.'});
       }
+    }
+  })
+
 });
 
 app.listen(4000, () => {
