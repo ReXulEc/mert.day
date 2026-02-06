47bb07b46bc119cfab0631de9bd9204e55baa29a
Mert Dogu <mertdoguonder@protonmail.com>
2026-02-06 15:41:02 +0300

    fix: bug

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,9 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      fallback: 'index.html',
+      strict: false
+    })
   }
 };
